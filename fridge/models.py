403f24a30ffa9a0330217283b2723409fd0da4c7
Artur Drzazga <artur.drzazga03@gmail.com>
2026-07-01 16:16:27 +0200

    Add Product model

--- fridge/models.py
+++ fridge/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
-# Create your models here.
+from config import settings
+
+
+class Product(models.Model):
+    STORAGE_CHOICES = [
+        ("fridge", "Fridge"),
+        ("freezer", "Freezer"),
+    ]
+
+    CATEGORY_CHOICES = [
+        ("dairy", "Dairy"),
+        ("meat", "Meat & Fish"),
+        ("veggies", "Vegetables & Fruits"),
+        ("other", "Other"),
+    ]
+
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="products",
+    )
+    name = models.CharField(max_length=50)
+    category = models.CharField(
+        max_length=30,
+        choices=CATEGORY_CHOICES,
+        default="other"
+    )
+    storage = models.CharField(
+        max_length=30,
+        choices=STORAGE_CHOICES,
+        default="fridge"
+    )
+    quantity = models.CharField(max_length=10)
+    expiry_date = models.DateField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.name} ({self.quantity}) - {self.user.email}"
