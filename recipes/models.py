a9acf4b09885541e86648b1d55c0a6ac584ed7b7
Artur Drzazga <artur.drzazga03@gmail.com>
2026-07-01 16:38:52 +0200

    Add SavedRecipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from config import settings
+
+
 class Recipe(models.Model):
     title = models.CharField(max_length=255)
     ingredients = models.JSONField(default=list)
@@ -10,3 +13,20 @@ class Recipe(models.Model):
         return self.title
 
 
+class SavedRecipe(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="recipes",
+    )
+    recipe = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name="recipes",
+    )
+
+    class Meta:
+        unique_together = ("user", "recipe")
+
+    def __str__(self):
+        return f"{self.user.email} saved {self.recipe.title}"
