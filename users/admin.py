8dcecb05910e4018455aa9a7d142c97ebd972c1b
Artur Drzazga <artur.drzazga03@gmail.com>
2026-07-01 15:31:58 +0200

    Implement custom user model

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class UserAdmin(UserAdmin):
+    list_display = ("email", "is_staff", "is_active", "created_at")
+    list_filter = ("is_staff", "is_superuser", "is_active")
+
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {
+            "fields": (
+                "is_active",
+                "is_staff",
+                "is_superuser",
+                "groups",
+                "user_permissions"
+                )
+            }
+        ),
+        ("Important dates", {"fields": ("last_login", "created_at")}),
+    )
+
+    readonly_fields = ("created_at", "last_login")
+
+    search_fields = ("email",)
+    ordering = ("email",)
--- users/models.py
+++ users/models.py
@@ -1,3 +1,18 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from users.managers import CustomUserManager
+
+
+class User(AbstractUser):
+    username = None
+    email = models.EmailField(unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = CustomUserManager()
+
+    def __str__(self):
+        return self.email
