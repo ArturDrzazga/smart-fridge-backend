49529ec4315be2496c50987276623a6d8b3d71e2
Artur Drzazga <artur.drzazga03@gmail.com>
2026-07-01 14:17:22 +0200

    Fix static files and update gitignore

--- config/urls.py
+++ config/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
@@ -21,6 +22,8 @@ from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+from config import settings
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -43,3 +46,6 @@ urlpatterns = [
         name="schema-swagger-ui",
     ),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
