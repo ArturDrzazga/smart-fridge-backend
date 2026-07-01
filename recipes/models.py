83cbc0a3af13e3dcd8cef77690d84fff00284601
Artur Drzazga <artur.drzazga03@gmail.com>
2026-07-01 16:35:27 +0200

    Add Recipe model

--- recipes/models.py
+++ recipes/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    ingredients = models.JSONField(default=list)
+    steps = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
