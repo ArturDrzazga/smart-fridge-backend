af784a3d47276034bcce30a2c0012ebc78e43c6d
Artur Drzazga <artur.drzazga03@gmail.com>
2026-07-02 13:41:06 +0200

    Setup Swagger UI using drf-spectacular

--- config/urls.py
+++ config/urls.py
@@ -18,32 +18,21 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path
 from django.urls.conf import include
-from drf_yasg import openapi
-from drf_yasg.views import get_schema_view
-from rest_framework import permissions
+from drf_spectacular.views import SpectacularAPIView, SpectacularSwaggerView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from config import settings
-
-schema_view = get_schema_view(
-    openapi.Info(
-        title="Smart Fridge API",
-        default_version="v1",
-        description="API documentation for Smart Fridge Management",
-    ),
-    public=True,
-    permission_classes=[permissions.AllowAny],
-)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include("fridge.urls", namespace="fridge")),
     path("api/token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
     path("api/token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
     path(
-        "swagger/",
-        schema_view.with_ui("swagger", cache_timeout=0),
-        name="schema-swagger-ui",
+        "api/docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui"
     ),
 ]
 
