278380e982e2fb0799a69592c71285e1900967a2
Artur Drzazga <artur.drzazga03@gmail.com>
2026-07-07 11:16:46 +0200

    feat: create ProductListAPIView

--- fridge/views.py
+++ fridge/views.py
@@ -1,7 +1,20 @@
+from rest_framework import generics
 from rest_framework.decorators import api_view
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+
+from fridge.models import Product
+from fridge.serializers import ProductSerializer
 
 
 @api_view(["GET"])
 def health_check(request):
     return Response({"status": "OK", "message": "Hello World"})
+
+
+class ProductListAPIView(generics.ListAPIView):
+    serializer_class = ProductSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Product.objects.filter(user=self.request.user)
